753663fe289a8018744d20c586741ad17f2b91d5
LulianoM <martinslucianofigueira@gmail.com>
2022-01-18 20:04:41 -0300

    final-commit-desgraça-de-projeto

---
+++ src/commands/populateProducts.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"ambassador/src/database"
+	"ambassador/src/models"
+	"math/rand"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+func populateProducts() {
+	database.Connect()
+
+	for i := 0; i < 30; i++ {
+		product := models.Product{
+			Title:       faker.Username(),
+			Description: faker.Username(),
+			Image:       faker.URL(),
+			Price:       float64(rand.Intn(90) + 10),
+		}
+
+		database.DB.Create(&product)
+	}
+}
---
+++ src/commands/populateUsers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"ambassador/src/database"
+	"ambassador/src/models"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+func populateUsers() {
+	database.Connect()
+
+	for i := 0; i < 30; i++ {
+		ambassador := models.User{
+			FirstName:    faker.FirstName(),
+			LastName:     faker.LastName(),
+			Email:        faker.Email(),
+			IsAmbassador: true,
+		}
+
+		ambassador.SetPassword("1234")
+
+		database.DB.Create(&ambassador)
+	}
+}
